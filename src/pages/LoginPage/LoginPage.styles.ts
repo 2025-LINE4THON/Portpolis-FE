30f4f9d9803c18e52e044007341cf5a21bf816f1
mzxxzysy <jsyjj21@gmail.com>
2025-11-09 00:48:48 +0900

    ✨ Feat: 로그인 페이지 퍼블리싱 (#3)

--- src/pages/LoginPage/LoginPage.styles.ts
+++ src/pages/LoginPage/LoginPage.styles.ts
@@ -1,7 +1,44 @@
 import styled from '@emotion/styled';
+import palette from '@/styles/theme';
 
-export const LoginPage = styled.div``;
+export const LoginPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 65px;
+  padding: 38px 40px;
+
+  border-radius: 22px;
+  box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.1);
+
+  width: 90%;
+  max-width: 560px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
 export const PwdBtn = styled.button`
   outline: none;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 57px;
+`;
+
+export const Text = styled.div`
+  font-size: 13px;
+  font-weight: 500;
+  color: ${palette.neutral.neutral300};
 `;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,9 @@
 import * as L from './LoginPage.styles';
+import Header from './components/Header/Header';
 import Button from './components/Button/Button';
 import InputBox from './components/InputBox/InputBox';
 import PwdIcon from '@assets/onBoarding/icon-pwd-shown.svg';
+import palette from '@/styles/theme';
 import { useState } from 'react';
 
 const LoginPage = () => {
@@ -9,24 +11,41 @@ const LoginPage = () => {
 
   return (
     <L.LoginPage>
-      <InputBox text="아이디" isPwd={false} placeholder="아이디를 입력해주세요." />
-      <InputBox
-        text="비밀번호"
-        isPwd={showPwd}
-        placeholder="비밀번호를 입력해주세요."
-        content={
-          <L.PwdBtn onClick={() => setShowPwd(!showPwd)}>
-            <img src={PwdIcon} />
-          </L.PwdBtn>
-        }
-      />
-      <Button
-        text="로그인"
-        isFilled={true}
-        onClick={() => {
-          console.log('로그인');
-        }}
-      />
+      <L.LoginBox>
+        <Header text="로그인" />
+        <L.InputContainer>
+          <InputBox text="아이디" isPwd={false} placeholder="아이디를 입력해주세요." />
+          <InputBox
+            text="비밀번호"
+            isPwd={showPwd}
+            placeholder="비밀번호를 입력해주세요."
+            content={
+              <L.PwdBtn onClick={() => setShowPwd(!showPwd)}>
+                <img src={PwdIcon} />
+              </L.PwdBtn>
+            }
+          />
+        </L.InputContainer>
+        <L.ButtonContainer>
+          <Button
+            text="로그인"
+            isFilled={true}
+            onClick={() => {
+              console.log('로그인');
+            }}
+          />
+          <L.Text>
+            아직 계정이 없으신가요?
+            <span
+              style={{ color: palette.primary.primary500, textDecoration: 'underline' }}
+              onClick={() => {
+                console.log('회원가입');
+              }}>
+              회원가입하기
+            </span>
+          </L.Text>
+        </L.ButtonContainer>
+      </L.LoginBox>
     </L.LoginPage>
   );
 };
