48dc927404986af12b957477ec9231b2895ad26f
jinhyo <kjhyo0220@gmail.com>
2025-11-09 16:17:55 +0900

    ✨ Feat: Navbar 구현 (#4)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import * as N from './Navbar.styles';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+
+  const LINKS = [
+    { id: 1, label: '내 커리어', to: '/career' },
+    { id: 2, label: '둘러보기', to: '/list' },
+    { id: 3, label: '포트폴리오', to: '/portfolio/create' },
+    { id: 4, label: '마이페이지', to: '/mypage' },
+  ];
+
+  return (
+    <N.Navbar>
+      <N.Logo onClick={() => navigate('/')}>Portpolis</N.Logo>
+
+      <N.List>
+        {LINKS.map((link) => (
+          <N.Menu key={link.id} to={link.to} className="b1">
+            <li>{link.label}</li>
+          </N.Menu>
+        ))}
+      </N.List>
+
+      <N.Empty />
+    </N.Navbar>
+  );
 };
 
 export default Navbar;
