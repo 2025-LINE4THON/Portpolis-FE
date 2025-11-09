ce776ed857cb2b866e51454be03d03b58d7f0f35
jinhyo <kjhyo0220@gmail.com>
2025-11-09 16:53:41 +0900

    ✨ Feat: 특정 경로에서 Navbar 보이지 않도록 (#4)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import * as N from './Navbar.styles';
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const LINKS = [
     { id: 1, label: '내 커리어', to: '/career' },
@@ -10,6 +11,14 @@ const Navbar = () => {
     { id: 3, label: '포트폴리오', to: '/portfolio/create' },
     { id: 4, label: '마이페이지', to: '/mypage' },
   ];
+
+  if (
+    pathname === '/login' ||
+    pathname === '/signup' ||
+    pathname.startsWith('/activity') ||
+    (pathname.startsWith(`/portfolio`) && pathname !== '/portfolio/create')
+  )
+    return null;
 
   return (
     <N.Navbar>
