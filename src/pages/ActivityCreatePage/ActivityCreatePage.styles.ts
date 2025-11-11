ba94fd7e7f43151e68e85b60bed5d2f0f8a7aef0
mzxxzysy <jsyjj21@gmail.com>
2025-11-12 03:56:35 +0900

    ✨ Feat: 헤더 이미지 업로드 구현 (#14)

--- src/pages/ActivityCreatePage/ActivityCreatePage.styles.ts
+++ src/pages/ActivityCreatePage/ActivityCreatePage.styles.ts
@@ -1,3 +1,75 @@
 import styled from '@emotion/styled';
+import palette from '@/styles/theme';
 
-export const ActivityCreatePage = styled.div``;
+export const ActivityCreatePage = styled.div`
+  position: relative;
+  bottom: 100px;
+`;
+
+export const Header = styled.div<{ bgimg: string | null }>`
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  border-radius: 20px;
+  filter: drop-shadow(0 4px 11px rgba(0, 0, 0, 0.14));
+  background-color: ${palette.neutral.neutral100};
+
+  ${({ bgimg }) =>
+    bgimg &&
+    `
+    background-image: url(${bgimg});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  `}
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 18px;
+  margin-top: 20px;
+`;
+
+export const GoBack = styled.img`
+  cursor: pointer;
+`;
+
+export const H3 = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 175%;
+`;
+
+export const AddFileBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 23px;
+  cursor: pointer;
+  margin-top: 50px;
+`;
+
+export const B2 = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+export const FileHandleBtns = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 10px;
+`;
+
+export const Btn = styled.button`
+  padding: 10px;
+  border-radius: 22px;
+  background: white;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  align-items: center;
+`;
+
+export const Body = styled.div``;
--- src/pages/ActivityCreatePage/ActivityCreatePage.tsx
+++ src/pages/ActivityCreatePage/ActivityCreatePage.tsx
@@ -1,7 +1,61 @@
 import * as A from './ActivityCreatePage.styles';
+import goback from '@assets/activity/icon-goback.svg';
+import Upload from '@assets/activity/icon-upload.svg?react';
+import trash from '@assets/activity/icon-trash.svg';
+import save from '@assets/activity/icon-edit-save.svg';
+import palette from '@/styles/theme';
+import { useState, useRef } from 'react';
 
 const ActivityCreatePage = () => {
-  return <A.ActivityCreatePage>ActivityCreatePage</A.ActivityCreatePage>;
+  const [file, setFile] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFileUploadClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (!selectedFile) return;
+
+    setFile(URL.createObjectURL(selectedFile));
+  };
+
+  return (
+    <A.ActivityCreatePage>
+      <A.Header bgimg={file}>
+        <A.Nav>
+          <A.GoBack src={goback} />
+          <A.H3>활동 등록</A.H3>
+        </A.Nav>
+
+        <A.AddFileBtn onClick={handleFileUploadClick}>
+          <Upload style={{ color: !file ? palette.neutral.neutral400 : palette.white }} />
+
+          <A.B2 style={{ color: !file ? palette.neutral.neutral400 : palette.white }}>대표 이미지 추가</A.B2>
+        </A.AddFileBtn>
+
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+
+        <A.FileHandleBtns>
+          <A.Btn>
+            저장하기 <img src={save} />
+          </A.Btn>
+          <A.Btn style={{ color: palette.danger.default }}>
+            삭제 <img src={trash} />
+          </A.Btn>
+        </A.FileHandleBtns>
+      </A.Header>
+
+      <A.Body></A.Body>
+    </A.ActivityCreatePage>
+  );
 };
 
 export default ActivityCreatePage;
