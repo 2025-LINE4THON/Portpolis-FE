c2cff0408f6a398ac9a8b7fe4f653812b6c16e60
jinhyo <kjhyo0220@gmail.com>
2025-11-11 18:51:15 +0900

    ✨ Feat: 소개 문구 수정 - 비주얼형 프로젝트 퍼블리싱 (#6)

--- src/pages/PortfolioCreatePage/components/EditProject/EditProject.tsx
+++ src/pages/PortfolioCreatePage/components/EditProject/EditProject.tsx
@@ -1,8 +1,10 @@
 import { usePortfolio } from '@/context/PortfolioContext';
 import * as E from './EditProject.styles';
 import EditIndexButton from '../EditIndexButton/EditIndexButton';
-import EditProjectStandardCard from '../../EditProjectStandardCard/EditProjectStandardCard';
+import EditProjectStandardCard from '../EditProjectStandardCard/EditProjectStandardCard';
 import Image from '@assets/PortfolioCreatePage/ex-card-image.png';
+import Imgae2 from '@assets/PortfolioCreatePage/ex-card-visual-image.png';
+import EditProjectVisualCard from '../EditProjectVisualCard/EditProjectVisualCard';
 
 const EditProject = () => {
   const { selectedTemplate, selectedProjects } = usePortfolio();
@@ -29,7 +31,20 @@ const EditProject = () => {
       )}
 
       {/* 비주얼형 */}
-      {selectedTemplate === 2 && <></>}
+      {selectedTemplate === 2 && (
+        <>
+          {selectedProjects.map((project, idx) => (
+            <EditProjectVisualCard
+              key={project.id}
+              idx={idx}
+              img={Imgae2}
+              date={project.date}
+              title={project.project}
+              stack={['FE', 'JAVA']}
+            />
+          ))}
+        </>
+      )}
     </E.EditProject>
   );
 };
---
+++ src/pages/PortfolioCreatePage/components/EditProjectVisualCard/EditProjectVisualCard.styles.ts
@@ -0,0 +1,127 @@
+import palette from '@/styles/theme';
+import styled from '@emotion/styled';
+
+export const EditProjectVisualCard = styled.div<{ $isImgLeft: boolean }>`
+  display: flex;
+  flex-direction: ${({ $isImgLeft }) => ($isImgLeft ? 'row' : 'row-reverse')};
+  align-items: ${({ $isImgLeft }) => ($isImgLeft ? 'start' : 'center')};
+  justify-content: center;
+
+  padding: 0 121px;
+`;
+
+export const ImgWrapper = styled.div<{ $isImgLeft: boolean }>`
+  width: 661px;
+  height: ${({ $isImgLeft }) => ($isImgLeft ? '480px' : '751px')};
+
+  border-radius: 22px;
+
+  position: relative;
+  z-index: 1;
+  isolation: isolate;
+
+  img:nth-of-type(2) {
+    position: absolute;
+    left: 40px;
+    top: -42px;
+    z-index: -1;
+
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    display: ${({ $isImgLeft }) => ($isImgLeft ? 'block' : 'none')};
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const InfoWrapper = styled.div<{ $isImgLeft: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${({ $isImgLeft }) => ($isImgLeft ? 'flex-end' : 'start')};
+  gap: 16px;
+
+  position: relative;
+  z-index: 2;
+
+  margin-left: ${({ $isImgLeft }) => ($isImgLeft ? '-80px' : '80px')};
+`;
+
+export const ProjectName = styled.div`
+  color: ${palette.neutral.neutral900};
+  font-size: 25px;
+  font-weight: 500;
+  line-height: 175%;
+`;
+
+export const Link = styled.div`
+  display: flex;
+  gap: 19px;
+  align-items: flex-end;
+`;
+
+export const Date = styled.div`
+  color: ${palette.neutral.neutral900};
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 175%;
+
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 25px;
+    background-color: ${palette.primary.primary100};
+  }
+`;
+
+export const ChipWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 9px;
+
+  position: absolute;
+  top: -17px;
+`;
+
+export const Chip = styled.div`
+  padding: 3px 18px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  border-radius: 22px;
+  background-color: #eaeaea;
+
+  color: ${palette.neutral.neutral700};
+`;
+
+export const ContentBox = styled.div`
+  padding: 30px 42px;
+  color: ${palette.neutral.neutral700};
+  text-align: right;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 175%;
+
+  border-radius: 22px;
+  background: ${palette.white};
+  box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.25);
+
+  position: relative;
+
+  min-width: 463px;
+  height: 159px;
+
+  margin-top: 18px;
+`;
