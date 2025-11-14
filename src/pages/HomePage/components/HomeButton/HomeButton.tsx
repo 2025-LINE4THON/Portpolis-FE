756cd8143ac733eecf61cb598a4080e74b0d41f2
jinhyo <kjhyo0220@gmail.com>
2025-11-14 16:28:40 +0900

    🐛 Fix: 홈 화면에서도 로그인 페이지 리다이렉션 추가 (#40)

--- src/pages/HomePage/components/HomeButton/HomeButton.tsx
+++ src/pages/HomePage/components/HomeButton/HomeButton.tsx
@@ -1,4 +1,8 @@
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 import * as H from './HomeButton.styles';
+import { LOCAL_STORAGE_KEY } from '@/constants/key';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface HomeButtonProps {
   Icon: React.ReactNode;
@@ -7,9 +11,22 @@ interface HomeButtonProps {
 }
 
 const HomeButton = ({ Icon, text, to }: HomeButtonProps) => {
+  const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [accessToken, setAccessToken] = useState(getItem());
+  const navigate = useNavigate();
+
   return (
     <H.HomeButton>
-      <H.StyledLink to={to}>
+      <H.StyledLink
+        to={to}
+        onClick={(e) => {
+          if (to !== '/signup' && !accessToken) {
+            e.preventDefault();
+            alert('로그인이 필요한 서비스입니다.');
+            navigate('/login');
+          }
+        }}>
         <H.IconWrapper>{Icon}</H.IconWrapper>
         <H.Menu>{text}</H.Menu>
       </H.StyledLink>
